2852e0301bd7a2a99189af511e23a344a28a901a
Anne O'Malley <pepper.seattle@gmail.com>
2018-07-22 01:33:02 -0700

    Reworked pixel art project to be less jquery heavy

--- projects/pam2/design.js
+++ projects/pam2/design.js
@@ -1,16 +1,19 @@
-const colorPicker = document.getElementById('#color-picker').value;
+const colorPicker = document.getElementById('#color-picker'),
+canvas = document.getElementById('#canvas'),
+submit = document.getElementById('#submit');
+
 
 //Make the Grid
 const makeGrid = () => {
   canvas.empty();
 
-  let rows = $('#inputX').val();
-  let columns = $('#inputY').val();
+  let rows = document.getElementById('#inputX').val();
+  let columns = document.getElementById('#inputY').val();
 
   let grid = [];
 
-  for(let r =0; r < rows; r++) {
-    grid.push('<tr');
+  for(let r = 0; r < rows; r++) {
+    grid.push('<tr>');
       for(let c = 0; c < columns; c++) {
         grid.push('<td></td>');
       }
@@ -18,3 +21,12 @@ const makeGrid = () => {
   }
   canvas.append(grid);
 }
+
+const runGrid = () => {
+  makeGrid();
+
+  submit.click(function (e) {
+    e.preventDefault();
+    makeGrid();
+  });
+};
